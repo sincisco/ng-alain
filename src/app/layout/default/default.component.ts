d5ce4e728e17271158a9e342ae09c152744419fb
李爱东 <1158889685@qq.com>
2018-01-19 20:36:37 +0800

    bug fix 修改内容区没有滚动条的问题

--- src/app/layout/default/default.component.ts
+++ src/app/layout/default/default.component.ts
@@ -1,21 +1,21 @@
-import { Component } from '@angular/core';
-import { Router, NavigationEnd, RouteConfigLoadStart, NavigationError } from '@angular/router';
-import { NzMessageService } from 'ng-zorro-antd';
-import { ScrollService, MenuService, SettingsService } from '@delon/theme';
+import {Component} from "@angular/core";
+import {Router, NavigationEnd, RouteConfigLoadStart, NavigationError} from "@angular/router";
+import {NzMessageService} from "ng-zorro-antd";
+import {ScrollService, MenuService, SettingsService} from "@delon/theme";
 
 @Component({
-    selector: 'layout-default',
-    templateUrl: './default.component.html'
+    selector: "layout-default",
+    templateUrl: "./default.component.html",
+    styleUrls: ["./default.component.less"]
 })
 export class LayoutDefaultComponent {
     isFetching = false;
 
-    constructor(
-        router: Router,
-        scroll: ScrollService,
-        private _message: NzMessageService,
-        public menuSrv: MenuService,
-        public settings: SettingsService) {
+    constructor(router: Router,
+                scroll: ScrollService,
+                private _message: NzMessageService,
+                public menuSrv: MenuService,
+                public settings: SettingsService) {
         // scroll to top in change page
         router.events.subscribe(evt => {
             if (!this.isFetching && evt instanceof RouteConfigLoadStart) {
@@ -23,7 +23,7 @@ export class LayoutDefaultComponent {
             }
             if (evt instanceof NavigationError) {
                 this.isFetching = false;
-                _message.error(`无法加载${evt.url}路由`, { nzDuration: 1000 * 3 });
+                _message.error(`无法加载${evt.url}路由`, {nzDuration: 1000 * 3});
                 return;
             }
             if (!(evt instanceof NavigationEnd)) {
