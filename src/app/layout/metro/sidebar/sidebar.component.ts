3a63e3cc8788d2d073cb650a0b876ee59fcb991a
李爱东 <1158889685@qq.com>
2018-01-20 18:39:44 +0800

    bug fix 修改metrolayout布局中sidebar样式   避免与defaultLayout的样式发生干扰

--- src/app/layout/metro/sidebar/sidebar.component.ts
+++ src/app/layout/metro/sidebar/sidebar.component.ts
@@ -2,12 +2,12 @@ import {Component, ElementRef, ViewEncapsulation} from '@angular/core';
 
 @Component({
     selector: 'app-sidebar',
-    encapsulation: ViewEncapsulation.None,
+    encapsulation: ViewEncapsulation.Emulated,
     templateUrl: './sidebar.component.html',
     styleUrls: ['./sidebar.component.less']
 })
 export class SidebarComponent {
     constructor(private _elementRef: ElementRef) {
-        $(_elementRef.nativeElement).addClass("aside");
+        $(_elementRef.nativeElement).addClass("aside-metro");
     }
 }
